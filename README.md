5ce657b2600d486209c34b2e05cdca97069d790d
Ni Xu <nixu09@gmail.com>
2017-05-05 16:12:29 -0400

    Inject jQuery. Updated readme.

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # download-all-cases-from-canvas
-How to (easily) download all cases from Canvas
+## How to (easily) download all cases from Canvas
+
+First, watch [Video Instructions] (https://vimeo.com/216230063)
+password: hbsftw
 
 * I only tested in Chrome.
 * Allow popup windows in Chrome.
-* Disable your Chrome PDF Viewer plugin (Type chrome://plugins/ in Chrome. See video instructions.)
-* You need to log in Canvas first.
+* Disable your Chrome PDF Viewer plugin (Type "chrome://settings/content" in Chrome. Scroll down to PDF Documents > Enable "Open PDF files in the default PDF viewer application". See video instructions.)
+* Log in Canvas first.
 * Go to each course's Overview page and run the script.
 * You need Chrome Developer Tools. (Don’t worry! I included in video instructions.)
 * Follow the weird steps to “click the hyperlink.” (See video instructions. I still don’t understand why I have to do this. May have something to do with DOM… not important. But if you figure it out, please let me know!)
+* Copy code, paste in console, and hit "Enter."
 
-[Video Instructions] (https://vimeo.com/196089072)
-password: hbsftw
--- case_download_js_v2.js
+++ case_download_js_v2.js
@@ -1,12 +1,21 @@
-var href;
-var re = new RegExp("^(http|https)://cb.hbsp.harvard.edu/cbmp/pl", "i"); 
-var re2 = new RegExp("^(http|https)://hbs.instructure.com/courses/", "i");  
-var re3 = new RegExp("^(http|https)://services.hbsp.harvard.edu/services", "i");  
+// download jQuery 
+var script = document.createElement('script');
+script.src = "https://code.jquery.com/jquery-3.2.1.min.js";
+document.getElementsByTagName('head')[0].appendChild(script);
 
-$("a").each(function() {
- 	  href = $( this ).attr( "href" );
- 	  if (re.test(href) || re2.test(href) || re3.test(href)){
- 	  	console.log(href);
- 	  	$( this ).click();
- 	  }
-});
+setTimeout(download, 3000);
+
+function download(){
+	var href;
+	var re = new RegExp("^(http|https)://cb.hbsp.harvard.edu/cbmp/pl", "i"); 
+	var re2 = new RegExp("^(http|https)://hbs.instructure.com/courses/", "i");  
+	var re3 = new RegExp("^(http|https)://services.hbsp.harvard.edu/services", "i");  
+
+	$("a").each(function() {
+	 	  href = $( this ).attr( "href" );
+	 	  if (re.test(href) || re2.test(href) || re3.test(href)){
+	 	  	console.log(href);
+	 	  	$( this )[0].click();
+	 	  }
+	});
+}
